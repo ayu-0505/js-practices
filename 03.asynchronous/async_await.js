6a41b5dea5c9808ec7294a5c6180e0a9c48eab98
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-06-21 17:07:01 +0900

    async/await版のエラーなしファイルを作成

---
+++ 03.asynchronous/async_await.js
@@ -0,0 +1,18 @@
+import sqlite3 from "sqlite3";
+import { promiseRun, promiseGet } from "./promise_functions.js";
+
+const db = new sqlite3.Database(":memory:");
+
+(async () => {
+  await promiseRun(
+    db,
+    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+  const state = await promiseRun(db, "INSERT INTO books(title) VALUES(?)", [
+    "吾輩は猫である(async/await版)",
+  ]);
+  console.log(`ID: ${state.lastID}`);
+  const data = await promiseGet(db, "SELECT * FROM books");
+  console.log(data);
+  db.close();
+})();
