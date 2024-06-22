105d5f4818f4bb3ddd764b4de7237c08dbfaf074
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-06-22 11:25:02 +0900

    プレースホルダー対象の文字列に[]を追加

--- 03.asynchronous/callback_error_handling.js
+++ 03.asynchronous/callback_error_handling.js
@@ -4,7 +4,7 @@ const db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
-    db.run("INSERT INTO books(titl) VALUES(?)", "吾輩は猫である", (err) => {
+    db.run("INSERT INTO books(titl) VALUES(?)", ["吾輩は猫である"], (err) => {
       // titleをtitlにして処理
       if (err) {
         console.log(err.message);
