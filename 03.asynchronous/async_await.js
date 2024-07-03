1a516e5fe7be69b32aa93f5f2e94045cfe6b3ba8
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-07-03 10:44:39 +0900

    実態を表現するように変数名を修正

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -7,10 +7,10 @@ await promiseRun(
   db,
   "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
 );
-const state = await promiseRun(db, "INSERT INTO books(title) VALUES(?)", [
+const details = await promiseRun(db, "INSERT INTO books(title) VALUES(?)", [
   "吾輩は猫である(async/await版)",
 ]);
-console.log(`ID: ${state.lastID}`);
+console.log(`ID: ${details.lastID}`);
 const row = await promiseGet(db, "SELECT * FROM books");
 console.log(row);
 db.close();
