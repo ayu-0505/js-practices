34050602dd7393527e9f32adc00e39f70caef961
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-07-03 15:53:52 +0900

    余計なコメントを削除

--- 03.asynchronous/callback_error_handling.js
+++ 03.asynchronous/callback_error_handling.js
@@ -19,21 +19,3 @@ db.run(
     });
   },
 );
-
-// db.run(
-//   "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-//   () => {
-//     db.run("INSERT INTO books(titl) VALUES(?)", ["吾輩は猫である"], (err) => {
-//       // titleをtitlにして処理
-//       if (err) {
-//         console.error(err.message);
-//       }
-//     }).get("SELECT * FROM book", (err) => {
-//       // booksをbookにして処理
-//       if (err) {
-//         console.error(err.message);
-//       }
-//       db.close();
-//     });
-//   },
-// );
