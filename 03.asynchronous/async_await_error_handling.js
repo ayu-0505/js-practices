bbe3f6d12e2091dfe10ffa8b531f0475359fc5b2
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-07-03 10:01:46 +0900

    プロミス化したDB関数をまとめたファイル名をpromise_functionsからpromisify_db_functionsに変更

--- 03.asynchronous/async_await_error_handling.js
+++ 03.asynchronous/async_await_error_handling.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { promiseRun, promiseGet } from "./promise_functions.js";
+import { promiseRun, promiseGet } from "./promisify_db_functions.js";
 
 const db = new sqlite3.Database(":memory:");
 
