e163c305d99808560e4e595d7a3b6506e0f43115
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-07-03 10:46:34 +0900

    async/awaitのエラーハンドリングにおいて即時実行関数からTop-level awaitに変更

--- 03.asynchronous/async_await_error_handling.js
+++ 03.asynchronous/async_await_error_handling.js
@@ -3,24 +3,22 @@ import { promiseRun, promiseGet } from "./promisify_db_functions.js";
 
 const db = new sqlite3.Database(":memory:");
 
-(async () => {
-  await promiseRun(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
-  try {
-    // titleをtitlにして処理
-    await promiseRun(db, "INSERT INTO books(titl) VALUES(?)", [
-      "吾輩は猫である(async/await版)",
-    ]);
-  } catch (error) {
-    console.error(error.message);
-  }
-  try {
-    // booksをbookにして処理
-    await promiseGet(db, "SELECT * FROM book");
-  } catch (error) {
-    console.error(error.message);
-  }
-  db.close();
-})();
+await promiseRun(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+try {
+  // titleをtitlにして処理
+  await promiseRun(db, "INSERT INTO books(titl) VALUES(?)", [
+    "吾輩は猫である(async/await版)",
+  ]);
+} catch (error) {
+  console.error(error.message);
+}
+try {
+  // booksをbookにして処理
+  await promiseGet(db, "SELECT * FROM book");
+} catch (error) {
+  console.error(error.message);
+}
+db.close();
