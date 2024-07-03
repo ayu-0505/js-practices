69bd0ed591ea866f6851d1b649b7ab22930fb1fd
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-07-03 10:42:24 +0900

    即時実行関数からTop-level awaitに変更

--- 03.asynchronous/async_await.js
+++ 03.asynchronous/async_await.js
@@ -3,16 +3,14 @@ import { promiseRun, promiseGet } from "./promisify_db_functions.js";
 
 const db = new sqlite3.Database(":memory:");
 
-(async () => {
-  await promiseRun(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
-  const state = await promiseRun(db, "INSERT INTO books(title) VALUES(?)", [
-    "吾輩は猫である(async/await版)",
-  ]);
-  console.log(`ID: ${state.lastID}`);
-  const row = await promiseGet(db, "SELECT * FROM books");
-  console.log(row);
-  db.close();
-})();
+await promiseRun(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+const state = await promiseRun(db, "INSERT INTO books(title) VALUES(?)", [
+  "吾輩は猫である(async/await版)",
+]);
+console.log(`ID: ${state.lastID}`);
+const row = await promiseGet(db, "SELECT * FROM books");
+console.log(row);
+db.close();
