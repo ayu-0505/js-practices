5c6c569d529074feb9e5eedf2bba3ea0242620be
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-07-10 10:23:56 +0900

    DB関係の関数ファイルの名称を変更

--- 03.asynchronous/async_await_error_handling.js
+++ 03.asynchronous/async_await_error_handling.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { promiseRun, promiseGet } from "./promisify_db_functions.js";
+import { promiseRun, promiseGet } from "./db_functions_wrapped_by_promise.js";
 
 const db = new sqlite3.Database(":memory:");
 
